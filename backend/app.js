47fda922419c7a1c12fe90959a3d7955a9f5b136
javierpb275 <javierpb275@gmail.com>
2021-07-11 19:49:40 +0200

    connected to mongodb database

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb').MongoClient;
 
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
@@ -24,5 +25,14 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
+
+mongodb.connect('mongodb+srv://maximilian:hqG9VedJmagiKhKo@cluster0-ntrwp.mongodb.net/shop?retryWrites=true')
+  .then(client => {
+    console.log('Connected!');
+    client.close();
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.listen(3100);
