5ab25976c58beedcdf5549e57bebb3e80e73392b
javierpb275 <javierpb275@gmail.com>
2021-07-12 23:01:39 +0200

    implementing pagination

--- src/pages/Product/Products.js
+++ src/pages/Product/Products.js
@@ -26,7 +26,7 @@ class ProductsPage extends Component {
 
   fetchData = () => {
     axios
-      .get('http://localhost:3100/products')
+      .get('http://localhost:3100/products')//?page=1
       .then(productsResponse => {
         this.setState({ isLoading: false, products: productsResponse.data });
       })
